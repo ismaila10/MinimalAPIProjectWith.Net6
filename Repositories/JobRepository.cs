3139f5144d5444458e089e957ef53bb75f957795
ismaila10 <lodia.ismaila@gmail.com>
2021-12-15 11:16:17 +0100

    Create Repositories and add List of objects

---
+++ Repositories/JobRepository.cs
@@ -0,0 +1,46 @@
+﻿using MinimalAPIProjectWith.Net6.Models;
+
+namespace MinimalAPIProjectWith.Net6.Repositories
+{
+    public class JobRepository
+    {
+        public static List<Job> Jobs = new()
+        {
+            new()
+            {
+                Id = 1,
+                Title = "Développeur",
+                Description = "Programming application with pc ",
+                Salary = 3000,
+            },
+            new()
+            {
+                Id = 2,
+                Title = "Designer",
+                Description = "Design application interface with pc ",
+                Salary = 2500,
+            },
+            new()
+            {
+                Id = 3,
+                Title = "Product Owner",
+                Description = "Responsable of product  ",
+                Salary = 2300,
+            },
+            new()
+            {
+                Id = 4,
+                Title = "Business Analyst",
+                Description = " Analyse the product value ",
+                Salary = 2800,
+            },
+            new()
+            {
+                Id = 5,
+                Title = "Database Administrator",
+                Description = " Modeling data conception",
+                Salary = 3200,
+            },
+        };
+    }
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿
+using MinimalAPIProjectWith.Net6.Models;
+
+namespace MinimalAPIProjectWith.Net6.Repositories
+{
+    public class UserRepository
+    {
+        public static List<User> Users = new()
+        {
+            new()
+            {
+                UserName = "ishlolo",
+                Email = "iso@hotmail.com",
+                Password = "Ishlolo123!",
+                FirstName = "ish",
+                LastName = "lolo",
+                Role = "Administrator"
+            },
+            new()
+            {
+                UserName = "iso",
+                Email = "ishlolo@hotmail.com",
+                Password = "Ishlolo123!",
+                FirstName = "iso",
+                LastName = "lolo",
+                Role = "Standard"
+            },
+        };
+    }
+}
